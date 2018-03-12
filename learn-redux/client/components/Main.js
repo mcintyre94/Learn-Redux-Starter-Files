4ba1ae3144ff824f0b8b2523365443305ca17a4f
mcintyre1994 <mcintyre1994@gmail.com>
2018-03-12 20:40:48 +0000

    Initial commit: add Main component with header
    - Using stateless functional component for now (instead of older React.createClass)

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Main = props => {
+  return (
+    <div>
+      <Link to="/">
+        <h1>Reduxstagram</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Main;
