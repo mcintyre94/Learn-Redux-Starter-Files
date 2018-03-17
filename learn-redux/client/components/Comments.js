88b0504f58bf1126c367e479ec00517541aced74
mcintyre1994 <mcintyre1994@gmail.com>
2018-03-17 15:04:02 +0000

    Implement the Single photo view
    - Displays the Photo component
    - Displays the (placeholder for now) Comments component

---
+++ learn-redux/client/components/Comments.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Comments = props => {
+  return <div className="comment">I'm the Comments!!</div>;
+};
+
+export default Comments;
--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Photo from "./Photo";
+import Comments from "./Comments";
 
 const Single = props => {
-  return <div className="single-photo">Single component</div>;
+  // Need index of the pos
+  const { posts, params } = props;
+  const i = posts.findIndex(post => post.code === params.postId);
+  const post = posts[i];
+  console.log(post);
+  return (
+    <div className="single-photo">
+      <Photo index={i} post={post} {...props} />
+      <Comments />
+    </div>
+  );
 };
 
 export default Single;
